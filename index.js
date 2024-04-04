c3e5744382aa31f6aa0c8d7a3a3be656ad482c62
Wambui <nderituwambui93@gmail.com>
2024-04-05 01:19:47 +0300

    event listinening lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was happy!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener(); // Call the outer function to execute addEventListener()
+  
